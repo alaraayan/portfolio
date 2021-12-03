fc22825f6b555df8bb6737b2d4f0f63ca684a3fb
alara <alaraayan@hotmail.com>
2021-12-03 21:11:12 +0000

    contact web done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
-import locationPin from '../assets/images/pin.png'
-import gitHub from '../assets/images/github.png'
-import linkedIn from '../assets/images/linkedin.png'
+// import locationPin from '../assets/images/pin.png'
+// import gitHub from '../assets/images/github.png'
+// import linkedIn from '../assets/images/linkedin.png'
 
 export default function Contact() {
   return (
@@ -8,16 +8,16 @@ export default function Contact() {
       <div className="component-container dark " id="contact"> 
         <div className="row-flex-container flex-row-centered">
           <h1 className="title-font section-title">Contact</h1>
-          <div className="contact-info-container">
-            <div className="icons row-flex-container">
-              <a href="https://github.com/alaraayan" target="_blank" rel="noreferrer" className="contact-links cursor"><img src={gitHub} alt="github icon" className="icon icon-white"></img><p>github.com/alaraayan</p></a>
+          <div className="contact-icons column-flex-container">    
+            <a href="https://github.com/alaraayan" target="_blank" rel="noreferrer" className="contact-links"><i className="devicon-github-original contact-icon"></i><p>github.com/alaraayan</p></a>
 
-              <a href="https://www.linkedin.com/in/alaraayan/" target="_blank" rel="noreferrer" className="contact-links" ><img src={linkedIn} alt="linkedin icon" className="icon icon-white"></img></a>
-              <div className="location-link-container "><img src={locationPin} alt="location pin icon" className="icon icon-white"></img>LONDON, N8</div>
-            </div>
+            <a href="https://www.linkedin.com/in/alaraayan/" target="_blank" rel="noreferrer" className="contact-links" ><i className="devicon-linkedin-plain contact-icon"></i><p>linkedin.com/in/alaraayan</p></a>
+
+            {/* <div className="location-link-container "></div> */}
           </div>
         </div>
       </div>
+
       {/* <hr className="section-break-left footer-break"/> */}
     </>
   )
