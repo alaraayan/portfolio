48a89f5dc250794dc79ec0e9ca6d096b87f7b2f8
alara <alaraayan@hotmail.com>
2021-12-03 01:59:01 +0000

    skill icons added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,46 @@
 export default function Skills() {
   return (
     <>
-      <div className="component-container light" id="skills"> LANGUAGES </div>
+      <div className="component-container light" id="skills"> 
+        <div className="column-flex-container separate">
+          <h1 className="title-font caps text-aligned-right">skills &amp; tools</h1>
+          <div className="skills-container">
+            <div className="skills languages">
+              Languages
+            </div>
+            <div className="skills tools">
+              Tools
+            </div>
+            <div className="skills studying">
+              Strudyi
+            </div>
+          </div>
+          {/* <div className="skills-icons">
+            <div className="icon-item"><i className="devicon-javascript-plain"></i></div>
+            <div className="icon-item"><i className="devicon-react-original-wordmark"></i></div>
+            <div className="icon-item"><i className="devicon-html5-plain-wordmark"></i></div>
+            <div className="icon-item"><i className="devicon-css3-plain-wordmark"></i></div>
+            <div className="icon-item"><i className="devicon-sass-original"></i></div>
+            <div className="icon-item"><i className="devicon-mongodb-plain-wordmark"></i></div>
+            <div className="icon-item"><i className="devicon-express-original"></i></div>
+            <div className="icon-item"><i className="devicon-nodejs-plain"></i></div>
+            <div className="icon-item"><i className="devicon-python-plain"></i></div>
+            <div className="icon-item"><i className="devicon-django-plain"></i></div>
+            <div className="icon-item"><i className="devicon-yarn-plain"></i></div>
+            <div className="icon-item"><i className="devicon-postgresql-plain"></i></div>
+            <div className="icon-item"><i className="devicon-vuejs-plain"></i></div>
+            <div className="icon-item"><i className="devicon-github-original"></i></div>
+            <div className="icon-item"><i className="devicon-git-plain"></i></div>
+            <div className="icon-item"><i className="devicon-visualstudio-plain"></i></div>
+            <div className="icon-item"><i className="devicon-heroku-original"></i></div>
+            <div className="icon-item"><i className="devicon-chrome-plain"></i></div>
+            <div className="icon-item"><i className="devicon-slack-plain"></i></div>
+            <div className="icon-item"><i className="devicon-trello-plain"></i></div>
+          </div> */}
+
+
+        </div>
+      </div>
       <hr className="section-break"/>
     </>
   )
