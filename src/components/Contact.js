ba88478285192622ea28cf5970e9676407a826e4
alara <alaraayan@hotmail.com>
2021-12-03 02:13:52 +0000

    contact layout done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
+import locationPin from '../assets/images/pin.png'
+import gitHub from '../assets/images/github.png'
+import linkedIn from '../assets/images/linkedin.png'
+
 export default function Contact() {
   return (
     <>
-      <div className="component-container dark " id="contact"> CONTACT </div>
+      <div className="component-container dark " id="contact"> 
+        <div className="row-flex-container flex-row-centered">
+          <h1 className="title-font section-title">Contact</h1>
+          <div className="contact-info-container">
+            <div className="icons row-flex-container">
+              <a href="https://github.com/alaraayan" target="_blank" rel="noreferrer" className="contact-links cursor"><img src={gitHub} alt="github icon" className="icon icon-white"></img><p>github.com/alaraayan</p></a>
+
+              <a href="https://www.linkedin.com/in/alaraayan/" target="_blank" rel="noreferrer" className="contact-links" ><img src={linkedIn} alt="linkedin icon" className="icon icon-white"></img></a>
+              <div className="location-link-container "><img src={locationPin} alt="location pin icon" className="icon icon-white"></img>LONDON, N8</div>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* <hr className="section-break-left footer-break"/> */}
     </>
   )
