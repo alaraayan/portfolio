1f413941f44cb5cc5959a6b4f1861f7731480617
alara <alaraayan@hotmail.com>
2021-12-14 07:31:55 +0000

    experience-text

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,7 +1,54 @@
 export default function WorkExperience() {
   return (
     <>
-      <div className="component-container light" id="experience"> WORK EXPERIENCE </div>
+      <div className="component-container light" id="experience"> 
+        <div className="column-flex-container">
+          <h1 className="title-font section-title">Previous Experience</h1>
+          <div className="timeline">
+            <div className="container left">
+              <div className="date">15 Dec</div>
+              <i className="icon fa fa-home"></i>
+              <div className="content">
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+                </p>
+              </div>
+            </div>
+            <div className="container right">
+              <div className="date">22 Oct</div>
+              <i className="icon fa fa-gift"></i>
+              <div className="content">
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+                </p>
+              </div>
+            </div>
+            <div className="container left">
+              <div className="date">10 Jul</div>
+              <i className="icon fa fa-user"></i>
+              <div className="content">
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+                </p>
+              </div>
+            </div>
+            <div className="container right">
+              <div className="date">18 May</div>
+              <i className="icon fa fa-running"></i>
+              <div className="content">
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>
+        Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      </div>
       <hr className="section-break"/>
     </>
   )
