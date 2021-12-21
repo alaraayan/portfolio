c82d49caa8211066c68cef33fbd4ffe50dda2fc2
alara <alaraayan@hotmail.com>
2021-12-21 17:19:41 +0000

    email added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,4 @@
-// import locationPin from '../assets/images/pin.png'
-// import gitHub from '../assets/images/github.png'
-// import linkedIn from '../assets/images/linkedin.png'
+// import EmailIcon from '@mui/icons-material/Email'
 
 export default function Contact() {
   return (
@@ -9,9 +7,18 @@ export default function Contact() {
         <div className="row-flex-container flex-row-centered">
           <h1 className="title-font section-title">Contact</h1>
           <div className="contact-icons column-flex-container">    
-            <a href="https://github.com/alaraayan" target="_blank" rel="noreferrer" className="contact-links"><i className="devicon-github-original contact-icon"></i><p>github.com/alaraayan</p></a>
-
-            <a href="https://www.linkedin.com/in/alaraayan/" target="_blank" rel="noreferrer" className="contact-links" ><i className="devicon-linkedin-plain contact-icon"></i><p>linkedin.com/in/alaraayan</p></a>
+            <a href="https://github.com/alaraayan" target="_blank" rel="noreferrer" className="contact-links">
+              <i className="devicon-github-original contact-icon"></i>
+              <p>github.com/alaraayan</p>
+            </a>
+            <a href="https://www.linkedin.com/in/alaraayan/" target="_blank" rel="noreferrer" className="contact-links" >
+              <i className="devicon-linkedin-plain contact-icon"></i>
+              <p>linkedin.com/in/alaraayan</p>
+            </a>
+            <a href="mailto:alaraayan@hotmail.com" target="_blank" rel="noreferrer" className="contact-links" >
+              <span className="material-icons contact-icon email-icon">email</span>
+              <p>alaraayan@hotmail.com</p>
+            </a>
 
             {/* <div className="location-link-container "></div> */}
           </div>
